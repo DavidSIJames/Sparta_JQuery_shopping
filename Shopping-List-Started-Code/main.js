0758db5cc2417cf1cf15f8a9d1998e9f6684c8f2
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-09-14 17:10:01 +0100

    Completed lab

--- Shopping-List-Started-Code/main.js
+++ Shopping-List-Started-Code/main.js
@@ -9,23 +9,34 @@ $(document).ready(function(){
     // on submit button click, text and shop selction are placed into variables
     let textInput = $('#addToList').val();
     let shopSel = $('.shop option:selected').val();
+    let newLi = "";
+    // switch statement to add text to correct shop list
     switch (shopSel) {
       case 'shop_1':
-        let newLi = $('<li class"list-item"></li>').text(textInput);
+         newLi = $('<li class="list-item"></li>').text(textInput);
         $(".shoppingList1").append(newLi);
-        e.prevent.default();
+        e.preventDefault();
         break;
       case 'shop_2':
-
+         newLi = $('<li class="list-item"></li>').text(textInput);
+        $(".shoppingList2").append(newLi);
+        e.preventDefault();
         break;
       case 'shop_3':
-
+        newLi = $('<li class="list-item"></li>').text(textInput);
+       $(".shoppingList3").append(newLi);
+       e.preventDefault();
         break;
       case 'shop_4':
-
+        newLi = $('<li class="list-item"></li>').text(textInput);
+       $(".shoppingList4").append(newLi);
+       e.preventDefault();
         break;
       default:
 
     }
+    $('.list-item').dblclick(function(){
+      $(this).fadeOut();
+    });
   });
 })
